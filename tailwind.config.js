968c5ba34f66564b55579bc65a318fad493b82c2
gabriellafaa <103623865+gabriellafaa@users.noreply.github.com>
2023-10-24 19:32:14 +0700

    memperbarui tailwinds

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ export default {
     "./resources/*/.blade.php",
     "./resources/*/.js",
     "./resources/*/.vue",
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
   ],
   theme: {
     extend: {

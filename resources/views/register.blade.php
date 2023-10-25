201c66f3b49138acbf18aed7fa04a3373271cce2
nimahmagfiroh <nmhmgfrh1@gmail.com>
2023-10-25 15:45:26 +0700

    Memperbaiki icon pada register  page

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -5,27 +5,27 @@
 <style>@import url('https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.3.45/css/materialdesignicons.min.css')</style>
 
 <!-- Header -->
-<header class="bg-cream-medium shadow-md">
-    <div class="flex justify-between items-center py-2 px-5 border-b-2 border-grey-200">
+<header class="bg-cream-medium shadow-md border-b-2 border-cream-medium">
+    <div class="flex justify-between items-center py-2 px-5">
         <div class="flex items-center">
         <!-- Logo -->
-        <img src="img/logomerah.png" alt="Bloodwave" class="w-20 h-16 ml-4">
+        <img src="img/logomerah.png" alt="Bloodwave" class="w-15 h-10 ml-4">
         </div>
 
         <!-- Navigasi Bar-->
         <div class="flex items-center space-x-2">
-        <a href="#" class="px-2 py-2 bg-read-dark text-white font-semibold font-semibold rounded-lg text-sm">Beranda</a>
-        <a href="#" class="px-2 py-2 bg-read-dark text-white font-semibold font-semibold rounded-lg text-sm">Riwayat Donor</a>
-        <a href="#" class="px-2 py-2 bg-read-dark text-white font-semibold font-semibold rounded-lg text-sm">Event</a>
-        <a href="#" class="px-2 py-2 bg-read-dark text-white font-semibold rounded-lg text-sm">Let's Read</a>
-        <a href="#" class="px-2 py-2 bg-red-dark text-cream-medium font-bold rounded-lg text-sm">Masuk</a>
+        <a href="#" class="px-2 py-2 bg-read-dark text-red-dark font-semibold font-semibold rounded-lg text-sm">Beranda</a>
+        <a href="#" class="px-2 py-2 bg-read-dark text-red-dark font-semibold font-semibold rounded-lg text-sm">Riwayat Donor</a>
+        <a href="#" class="px-2 py-2 bg-read-dark text-red-dark font-semibold font-semibold rounded-lg text-sm">Event</a>
+        <a href="#" class="px-2 py-2 bg-read-dark text-red-dark font-semibold rounded-lg text-sm">Let's Read</a>
+        <a href="#" class="px-2 py-2 bg-red-dark text-cream-medium hover:bg-cream-dark font-bold rounded-lg text-sm">Masuk</a>
         </div>
     </div>
 </header>
-    <div class="bg-cream-medium text-white-500 rounded-3xl shadow-xl w-full h-screen overflow-hidden">
-        <div class="md:flex w-full h-screen">
-            <div class="hidden md:block w-full h-screen">
-            <img src="img/bgregister.jpg">
+    <div class="bg-cream-medium text-white-500 w-full h-screen overflow-hidden">
+        <div class="hidden md:flex w-full h-screen">
+            <div class="hidden md:block w-full h-screen py-20">
+            <img src="img/bgregister.png">
         </div>
             <div class="w-full md:w-full py-10 px-5 md:px-10">
                 <div class="text-center mb-10">
@@ -46,7 +46,7 @@
                         <div class="w-full px-3 mb-5">
                             <label for="" class="text-xs font-semibold px-1">Nama</label>
                             <div class="flex">
-                                <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-email-outline text-gray-400 text-lg"></i></div>
+                                <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-Account-outline text-gray-400 text-lg"></i></div>
                                 <input type="nama" class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="Nimahahaha">
                             </div>
                         </div>
@@ -55,7 +55,7 @@
                         <div class="w-full px-3 mb-5">
                             <label for="" class="text-xs font-semibold px-1">Nomor Telepon</label>
                             <div class="flex">
-                                <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-email-outline text-gray-400 text-lg"></i></div>
+                                <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-Phone-outline text-gray-400 text-lg"></i></div>
                                 <input type="Nomor Telepon" class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="08123456789">
                             </div>
                         </div>
@@ -64,8 +64,8 @@
                         <div class="w-full px-3 mb-5">
                             <label for="" class="text-xs font-semibold px-1">Umur</label>
                             <div class="flex">
-                                <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-email-outline text-gray-400 text-lg"></i></div>
-                                <input type="Umur" class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="20 tahun">
+                                <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-Face-outline text-gray-400 text-lg"></i></div>
+                                <input type="Umur" class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="20">
                             </div>
                         </div>
                     </div>
@@ -80,7 +80,7 @@
                     </div>
                     <div class="flex -mx-3">
                         <div class="w-full px-3 mb-5">
-                            <button class="block w-full max-w-xs mx-auto bg-red-dark hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-bold">REGISTER NOW</button>
+                            <button class="block w-full max-w-xs mx-auto bg-red-dark hover:bg-cream-dark focus:bg-red-dark text-white rounded-lg px-3 py-3 font-bold">REGISTER NOW</button>
                         </div>
                     </div>
                 </div>

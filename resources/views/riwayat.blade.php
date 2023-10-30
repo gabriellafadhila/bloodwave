2746b61c9703f286b47b156068e54bc0fb27d82e
gabriellafaa <103623865+gabriellafaa@users.noreply.github.com>
2023-10-30 20:12:18 +0700

    membuata page riwayat

---
+++ resources/views/riwayat.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en"> <html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <title>Bloodwave - Darah Anda, Harapan Mereka</title>
+</head>
+<body class="bg-gray-100">
+
+<!-- Header -->
+<header class="bg-red-dark shadow-md">
+    <div class="flex justify-between items-center py-2 px-5 border-b-2 border-grey-200">
+        <div class="flex items-center">
+        <!-- Logo -->
+        <img src="img/logo.png" alt="Bloodwave" class="w-28 h-12 ml-6">
+        </div>
+
+        <!-- Navigasi Bar-->
+        <div class="flex items-center space-x-2">
+        <a href="#" class="px-2 py-2 bg-red-dark text-cream-medium font-semibold rounded-lg text-sm">Beranda</a>
+        <a href="#" class="px-2 py-2 bg-red-dark text-cream-medium font-semibold rounded-lg text-sm">Riwayat Donor</a>
+        <a href="#" class="px-2 py-2 bg-red-dark text-cream-medium font-semibold rounded-lg text-sm">Event</a>
+        <a href="#" class="px-2 py-2 bg-red-dark text-cream-medium font-semibold rounded-lg text-sm">Let's Read</a>
+        <a href="#" class="px-2 py-2 bg-cream-medium text-red-dark font-semibold rounded-lg text-sm">Masuk</a>
+        <a href="#" class="px-2 py-2 bg-cream-medium text-red-dark font-semibold rounded-lg text-sm">Daftar</a>
+        </div>
+    </div>
+</header>

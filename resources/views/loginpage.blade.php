856bf6a343db8b7e15f74f067823a15a54325d21
gabriellafaa <103623865+gabriellafaa@users.noreply.github.com>
2023-11-04 12:45:44 +0700

    mengedit header pada log in dan register

--- resources/views/loginpage.blade.php
+++ resources/views/loginpage.blade.php
@@ -17,12 +17,12 @@
 
         <!-- Navigasi Bar-->
         <div class="flex items-center space-x-2">
-            <a href="{{url('home')}}" class="px-2 py-2 bg-red-dark text-cream-medium font-semibold rounded-lg text-sm">Beranda</a>
-            <a href="#" class="px-2 py-2 bg-red-dark text-cream-medium font-semibold rounded-lg text-sm">Riwayat Donor</a>
-            <a href="#" class="px-2 py-2 bg-red-dark text-cream-medium font-semibold rounded-lg text-sm">Event</a>
-            <a href="#" class="px-2 py-2 bg-red-dark text-cream-medium font-semibold rounded-lg text-sm">Let's Read</a>
-            <a href="{{url('login')}}" class="px-2 py-2 bg-cream-medium text-red-dark font-semibold rounded-lg text-sm">Masuk</a>
-            <a href="{{url('register')}}" class="px-2 py-2 bg-cream-medium text-red-dark font-semibold rounded-lg text-sm">Daftar</a>
+            <a href="{{url('home')}}" class="px-2 py-2 bg-cream-medium text-red-dark font-semibold rounded-lg text-sm">Beranda</a>
+            <a href="#" class="px-2 py-2 bg-cream-medium text-red-dark font-semibold rounded-lg text-sm">Riwayat Donor</a>
+            <a href="#" class="px-2 py-2 bg-cream-medium text-red-dark font-semibold rounded-lg text-sm">Event</a>
+            <a href="#" class="px-2 py-2 bg-cream-medium text-red-dark font-semibold rounded-lg text-sm">Let's Read</a>
+            <a href="{{url('login')}}" class="px-2 py-2 bg-red-dark text-cream-medium font-semibold rounded-lg text-sm">Masuk</a>
+            <a href="{{url('register')}}" class="px-2 py-2 bg-red-dark text-cream-medium font-semibold rounded-lg text-sm">Daftar</a>
         </div>
     </div>
 </header>
